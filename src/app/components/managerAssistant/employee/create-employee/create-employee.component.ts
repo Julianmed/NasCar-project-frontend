9302045e61b1e1d39a022f396d7f7d7bf2474b97
alejandro.perez1 <alejandro.perez1@udea.edu.co>
2021-01-21 15:26:20 -0500

    create employee

--- src/app/components/managerAssistant/employee/create-employee/create-employee.component.ts
+++ src/app/components/managerAssistant/employee/create-employee/create-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EmployeeService } from '@app/services/employee/employee.service';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  constructor() { }
+  formCreateEmployee: FormGroup;
+  profileStatus: string[] = ["inactive", "active"];
+  roles: string[] = ['technician','HR assistant', 'Manager assistant'];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {this.formCreateEmployee = this.formBuilder.group({
+    _id: ['', Validators.required],
+    userID: ['', Validators.required],
+    user: ['', Validators.required],
+    email: ['', Validators.compose([Validators.email, Validators.required])],
+    fname: ['', Validators.required],
+    lname: ['', Validators.required],
+    cellphone: ['', Validators.required],
+    landline: ['', Validators.required],
+    role: ['', Validators.required],
+    photo: ['', Validators.required],
+    profileStatus: ['', Validators.required]
+  })
+}
 
   ngOnInit(): void {
+  }
+
+  registerEmployee(values){
+    console.log(this.formCreateEmployee.value);
+    this.employeeService.createEmployee(this.formCreateEmployee.value).subscribe(result => {
+      console.log(result);
+    });
+    this.router.navigate(['manager/profile']);
   }
 
 }
